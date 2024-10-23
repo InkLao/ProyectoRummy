d55dba031f5ce85b05c10adf2e9aaafa0642b7d4
stae01 <166432863+stae01@users.noreply.github.com>
2024-10-23 15:20:08 -0700

    Creación

---
+++ Rummy/src/main/java/com/mycompany/rummy/Rummy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rummy;
+
+/**
+ *
+ * @author user
+ */
+public class Rummy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
